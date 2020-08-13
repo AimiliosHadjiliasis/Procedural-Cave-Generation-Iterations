cececf22c69aee17784ce8bd5dba5cf1ac6e1edd
Aimilios <aimilioshadjiliasis@gmail.com>
2020-08-13 23:17:38 +0300

    Add the 3rd Iteration folder

---
+++ Iteration 3 - Implementation of the Cave's Mesh/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    //keep track of how much will be occubied by walls 
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    //Create the map (2D array of integers) which defines the a grid of integers 
+    //and any tile that is equal to 0 in the map will be an empty tile
+    //and any tile that is equal to 1 will be a tile that represents a wall
+    int[,] map;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap()
+    {
+        //Fill the map with random values as a starting configurations
+        //This is how cecullar automata works
+        map = new int[width, height];
+        RandomFillMap();
+
+        //We can use diferent number here to get different values for smoothing
+        for (int i = 0; i < 5; i++)
+        {
+            SmoothMap();
+        }
+
+        //Call the method mesh generator from the other class
+        //in order to generate the mesh
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(map, 1);
+    }
+
+    //method work by the seed
+    void RandomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        //pseudo random generator
+        //returns a unique hash code (an integer for the seed)
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        //Loop through the tiles of the map
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                //Set walls = 1 // set empty space = 0
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    //Smoothing iteration to generate walls
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                //Rules for walls:
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
+
+                if (neighbourWallTiles > 4)
+                    map[x, y] = 1;
+                else if (neighbourWallTiles < 4)
+                    map[x, y] = 0;
+
+            }
+        }
+    }
+
+    //Method that tell us how many neighbouring tiles are walls	
+    int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                //Inside the border
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height)
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map[neighbourX, neighbourY];
+                    }
+                }
+                //Outside the border	
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
+    //Method that draws in the Gizmos Property of unity in 
+    //order to see if everything is working
+
+    void OnDrawGizmos()
+    {
+        /*
+		if (map != null) {
+			for (int x = 0; x < width; x ++) {
+				for (int y = 0; y < height; y ++) {
+					Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+					Vector3 pos = new Vector3(-width/2 + x + .5f,0, -height/2 + y+.5f);
+					Gizmos.DrawCube(pos,Vector3.one);
+				}
+			}
+		}
+		*/
+    }
+
+}
---
+++ Iteration 3 - Implementation of the Cave's Mesh/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,300 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MeshGenerator : MonoBehaviour
+{
+
+    public SquareGrid squareGrid;
+
+    List<Vector3> vertices; //list of points
+    List<int> triangles;
+
+    public void GenerateMesh(int[,] map, float squareSize)
+    {
+        //set square grid variable
+        squareGrid = new SquareGrid(map, squareSize);
+
+        //initialise the lists:
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+        {
+            for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+            {
+                TriangulateSquare(squareGrid.squares[x,y]);
+            }
+        }
+
+        //Create the mesh
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+
+    }
+
+    void TriangulateSquare(Square square)
+    {
+        switch (square.configuration)
+        {
+            //Cases made from 0 points = No mesh
+            case 0: 
+                break; //no need to do anything
+
+            //all cases from 1 point selected
+            case 1:
+                MeshFromPoints(square.centreBottom, square.bottomLeft, square.centreLeft);
+                break;
+            case 2:
+                MeshFromPoints(square.centreRight, square.bottomRight, square.centreBottom);
+                break;
+            case 4:
+                MeshFromPoints(square.centreTop, square.topRight, square.centreRight);
+                break;
+            case 8:
+                MeshFromPoints(square.topLeft, square.centreTop, square.centreLeft);
+                break;
+            //all cases from 2 point selected
+            case 3:
+                MeshFromPoints(square.centreRight, square.bottomRight, square.bottomLeft, square.centreLeft);
+                break;
+            case 6:
+                MeshFromPoints(square.centreTop, square.topRight, square.bottomRight, square.centreBottom);
+                break;
+            case 9:
+                MeshFromPoints(square.topLeft, square.centreTop, square.centreBottom, square.bottomLeft);
+                break;
+            case 12:
+                MeshFromPoints(square.topLeft, square.topRight, square.centreRight, square.centreLeft);
+                break;
+            case 5:
+                MeshFromPoints(square.centreTop, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft, square.centreLeft);
+                break;
+            case 10:
+                MeshFromPoints(square.topLeft, square.centreTop, square.centreRight, square.bottomRight, square.centreBottom, square.centreLeft);
+                break;
+
+            //all cases from 3 point selected
+            case 7:
+                MeshFromPoints(square.centreTop, square.topRight, square.bottomRight, square.bottomLeft, square.centreLeft);
+                break;
+            case 11:
+                MeshFromPoints(square.topLeft, square.centreTop, square.centreRight, square.bottomRight, square.bottomLeft);
+                break;
+            case 13:
+                MeshFromPoints(square.topLeft, square.topRight, square.centreRight, square.centreBottom, square.bottomLeft);
+                break;
+            case 14:
+                MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.centreBottom, square.centreLeft);
+                break;
+
+            // all cases from 4 point selected
+            case 15:
+                MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.bottomLeft);
+                break;
+        
+
+
+        }
+    }
+
+    //params keyword specifies an unknown number of Nodes
+    void MeshFromPoints(params Node[] points)
+    {
+        //Make sure that tehy are assign to the list of vertices
+        AssignVertices(points);
+
+        //Create trianges of the points
+        //triangle needs at least 3 points
+        if (points.Length >= 3)
+            CreateTriangle(points[0], points[1], points[2]);
+        if (points.Length >= 4)
+            CreateTriangle(points[0], points[2], points[3]);
+        if (points.Length >= 5)
+            CreateTriangle(points[0], points[3], points[4]);
+        if (points.Length >= 6) //happens only in the diagonal
+            CreateTriangle(points[0], points[4], points[5]);
+    }
+
+    //Method that endures that our points have value
+    void AssignVertices(Node[] points)
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            //if our point has vertex index
+            if (points[i].vertexIndex == -1)    //-1 by default (empty)
+            {
+                points[i].vertexIndex = vertices.Count; //initialise size
+                vertices.Add(points[i].position);   //add vertex to vertices list
+            }
+        }
+    }
+
+    //Method that creates the triangles of the mesh
+    void CreateTriangle(Node a, Node b, Node c)
+    {
+        triangles.Add(a.vertexIndex);
+        triangles.Add(b.vertexIndex);
+        triangles.Add(c.vertexIndex);
+    }
+
+    //Method that draws in the gizmos our current version of implementation
+    void OnDrawGizmos()
+    {
+        /*
+        if (squareGrid != null)
+        {
+            for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+            {
+                for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+                {
+
+                    Gizmos.color = (squareGrid.squares[x, y].topLeft.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(squareGrid.squares[x, y].topLeft.position, Vector3.one * .4f);
+
+                    Gizmos.color = (squareGrid.squares[x, y].topRight.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(squareGrid.squares[x, y].topRight.position, Vector3.one * .4f);
+
+                    Gizmos.color = (squareGrid.squares[x, y].bottomRight.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(squareGrid.squares[x, y].bottomRight.position, Vector3.one * .4f);
+
+                    Gizmos.color = (squareGrid.squares[x, y].bottomLeft.active) ? Color.black : Color.white;
+                    Gizmos.DrawCube(squareGrid.squares[x, y].bottomLeft.position, Vector3.one * .4f);
+
+
+                    Gizmos.color = Color.grey;
+                    Gizmos.DrawCube(squareGrid.squares[x, y].centreTop.position, Vector3.one * .15f);
+                    Gizmos.DrawCube(squareGrid.squares[x, y].centreRight.position, Vector3.one * .15f);
+                    Gizmos.DrawCube(squareGrid.squares[x, y].centreBottom.position, Vector3.one * .15f);
+                    Gizmos.DrawCube(squareGrid.squares[x, y].centreLeft.position, Vector3.one * .15f);
+
+                }
+            }
+        }
+        */
+    }
+
+    public class SquareGrid
+    {
+        /***************************************************/
+        //Class that represents the square grid in our mesh
+        //which holds a 2d array of swquares	
+        /***************************************************/
+
+        public Square[,] squares;
+
+        public SquareGrid(int[,] map, float squareSize)
+        {
+            int nodeCountX = map.GetLength(0);
+            int nodeCountY = map.GetLength(1);
+            float mapWidth = nodeCountX * squareSize;
+            float mapHeight = nodeCountY * squareSize;
+
+            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+
+            for (int x = 0; x < nodeCountX; x++)
+            {
+                for (int y = 0; y < nodeCountY; y++)
+                {
+                    //Calculate position of control node
+                    Vector3 pos = new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2,
+                                               0,
+                                               -mapHeight / 2 + y * squareSize + squareSize / 2);
+
+                    //wall if its == 1 (active)
+                    controlNodes[x, y] = new ControlNode(pos, map[x, y] == 1, squareSize);
+                }
+            }
+
+            squares = new Square[nodeCountX - 1, nodeCountY - 1];
+            for (int x = 0; x < nodeCountX - 1; x++)
+            {
+                for (int y = 0; y < nodeCountY - 1; y++)
+                {
+                    squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
+                }
+            }
+
+        }
+    }
+
+    public class Square
+    {
+        /***********************************************/
+        //Class that represents every Square in our mesh
+        /***********************************************/
+
+        //Reference the control nodes (edges of square)
+        //and node (mid points of the squares)
+        public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+        public Node centreTop, centreRight, centreBottom, centreLeft;
+        public int configuration; //16 possible configurations fro control nodes on/off -> representing a binary num eg 0000
+
+        //Constructor:
+        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
+        {
+            //Set Control Nodes:
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomRight = _bottomRight;
+            bottomLeft = _bottomLeft;
+
+            //Set nodes:
+            centreTop = topLeft.right;
+            centreRight = bottomRight.above;
+            centreBottom = bottomLeft.right;
+            centreLeft = bottomLeft.above;
+
+            if (topLeft.active)
+                configuration += 8; //1000
+            if (topRight.active)
+                configuration += 4; //0100
+            if (bottomRight.active)
+                configuration += 2; //0010
+            if (topLeft.active)
+                configuration += 1; //0001
+
+        }
+    }
+
+    public class Node
+    {
+        /***********************************************/
+        //Class that represents every node in our mesh
+        /***********************************************/
+
+        public Vector3 position;    //Keep track of its position in the world
+        public int vertexIndex = -1;    //Keep track of the index
+
+        //Constructor:
+        public Node(Vector3 _pos)
+        {
+            position = _pos;
+        }
+    }
+
+    public class ControlNode : Node
+    {
+        /***********************************************/
+        //  Class that represents every 
+        //  switch (control node) in our mesh
+        /***********************************************/
+
+        public bool active; //active = wall // not active = emplty tile	
+        public Node above, right;
+
+        //Constructor:
+        //position is set by base constructor
+        public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos)
+        {
+            active = _active;
+            above = new Node(position + Vector3.forward * squareSize / 2f);
+            right = new Node(position + Vector3.right * squareSize / 2f);
+        }
+
+    }
+}
